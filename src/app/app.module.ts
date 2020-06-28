6a59d899e23a85f58e78913d243537490ef7f374
Leedabord <gwforumlakers@gmail.com>
2020-06-28 01:22:32 -0700

    4 files modified, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,3 +35,39 @@ import { Tab2aPage } from './tab2a/tab2a.page';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+/* 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NewContactPage } from './new-contact/new-contact.page';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, NewContactPage],
+  entryComponents: [NewContactPage],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+*/
