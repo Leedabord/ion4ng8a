e5cb5a0bf30fc36cf045e038ae6e188519e1195e
Leedabord <gwforumlakers@gmail.com>
2020-06-28 00:42:22 -0700

    2 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PopoverController} from '@ionic/angular';
+import { PopoverController, ToastController } from '@ionic/angular';
 import { Tab2aPage } from '../tab2a/tab2a.page';
 
 @Component({
@@ -9,7 +9,12 @@ import { Tab2aPage } from '../tab2a/tab2a.page';
 })
 export class Tab1Page {
 
-  constructor(public popoverController: PopoverController) {}
+  constructor(
+    public popoverController: PopoverController,
+    public toastCtrl: ToastController
+    ) {
+
+  }
 
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
@@ -21,4 +26,60 @@ export class Tab1Page {
     return await popover.present();
   }
 
+  async presentToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptatibus quibusdam eum nihil optio, ullam accusamus magni, nobis suscipit reprehenderit, sequi quam amet impedit. Accusamus dolorem voluptates laborum dolor obcaecati.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptatibus quibusdam eum nihil optio, ullam accusamus magni, nobis suscipit reprehenderit, sequi quam amet impedit. Accusamus dolorem voluptates laborum dolor obcaecati.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptatibus quibusdam eum nihil optio, ullam accusamus magni, nobis suscipit reprehenderit, sequi quam amet impedit. Accusamus dolorem voluptates laborum dolor obcaecati.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptatibus quibusdam eum nihil optio, ullam accusamus magni, nobis suscipit reprehenderit, sequi quam amet impedit. Accusamus dolorem voluptates laborum dolor obcaecati.',
+      position: 'middle',
+      duration: 3000
+    });
+    toast.present();
+  }
+
 }
+
+/* 
+  showToastWithCloseButton() {
+    const toast = this.toastCtrl.create({
+      message: 'Your internet connection appears to be offline. Data integrity is not guaranteed.',
+      showCloseButton: true,
+      closeButtonText: 'Ok'
+    });
+    toast.onDidDismiss(this.dismissHandler);
+    toast.present();
+  }
+
+  private dismissHandler() {
+    console.log('Toast onDidDismiss()');
+  }
+
+  constructor(public toastController: ToastController) {}
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
+}
+
+
+*/
