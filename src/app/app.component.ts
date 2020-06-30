ea5c294b0b03b9faa8249f90a2ee93271b251955
Leedabord <gwforumlakers@gmail.com>
2020-06-30 13:32:30 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,25 @@ import { Platform } from '@ionic/angular';
 })
 
 export class AppComponent {
+
+readonly favQuotesURL = 'https://api.airtable.com/v0/app0hohtq4b1nM0Kb/FavQuotes?api_key=key66fQg5IghIIQmb';
+
+quotes: any;
+
   constructor(
+    private httpC: HttpClient, 
     private platform: Platform
   ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
     this.platform.ready().then(() => {
+  //    this.initializeApp();
+    this.quotes = this.httpC.get(this.favQuotesURL);
     });
   }
+//  initializeApp() {  }
+
+  getQuotes() {  
+// return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+    this.quotes = this.httpC.get(this.favQuotesURL);
+  }
+
 }
